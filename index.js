25105ced1f74f763968c2fd79a4d42bb600bdef7
maddicoats <mcoats799@gmail.com>
2022-07-24 18:01:46 +0930

    fixed imageLinks error added styling

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 document.querySelector('#search-button').addEventListener("click", event => {
     return (
         console.log(document.querySelector('#search-input').value),
-        getBooks()
+        getBooks(),
+        document.querySelector("header").classList.remove("height")
         )
 })
+
+document.querySelector("#search-input").addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        document.querySelector("#search-button").click();
+    }
+});
 
 async function getBooks(event) {
     const input = document.querySelector('#search-input').value;
@@ -11,37 +18,49 @@ async function getBooks(event) {
     const response = await fetch(`https://www.googleapis.com/books/v1/volumes?q=${input}`);
 
     const data = await response.json();
-    //const book = data.items[0].volumeInfo
-
-    // book.imageLinks.smallThumbnail = document.querySelector('.book--image').src
-    // book.title = document.querySelector('.book--title').innerHTML
-    // book.authors = document.querySelector('.book--author').innerHTML
+    
     console.log(data)
     let output = '';
     data.items.forEach(book => {
-        console.log(book.volumeInfo.imageLinks.smallThumbnail)
+        console.log(book.volumeInfo.imageLinks)
         output += `
             <div class="bookcard">
-            <ul>
-                <img class="bookcard--img" src=${book.volumeInfo.imageLinks.smallThumbnail}>
-                
-                <li class="bookcard--title">${book.volumeInfo.title}</li>
+                <div>
+                    <img class="bookcard--img" src=${
+                        book.volumeInfo.imageLinks === undefined || null 
+                            ? "./not-found.png" 
+                            : `${book.volumeInfo.imageLinks.thumbnail}`
+                        
+                        // book.volumeInfo.imageLinks.thumbnail ? book.volumeInfo.imageLinks.thumbnail : "./not-found.png"
+                    }>
+                    
+                    <div><a href=${book.volumeInfo.canonicalVolumeLink} target="_blank"><button class="bookcard--button">more info</button></a></div>
+                </div>
 
-                <liclass="bookcard--author">${book.volumeInfo.authors}</li>
+                <div>
+                    <h3 class="bookcard--title">${book.volumeInfo.title}</h3>
 
-                <div><a href=${book.volumeInfo.canonicalVolumeLink}><button class="bookcard--button">details</button></a></div>
-            </ul>
+                    <h4 class="bookcard--author">${
+                        book.volumeInfo.authors ? "by " + book.volumeInfo.authors.join(', ') : "<i>author unavailable</i>"
+                    }</h4>
+
+                    <p class="bookcard--description">${
+                        book.volumeInfo.description ? book.volumeInfo.description : "<i>description unavailable</i>"
+
+                        //, book.volumeInfo.description.length>430 ? book.volumeInfo.description = book.volumeInfo.description.substr(0,430)+"..." : book.volumeInfo.description
+                        
+                    }</p>
+                </div>
             </div>
         `;
+
     });
 
-    // Display the search result
+    // display the results:
     document.querySelector("#output").innerHTML = output;
+
+    
 
 }
 
-document.querySelector("#search-input").addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        document.querySelector("#search-button").click();
-    }
-});
+
